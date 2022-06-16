dba16c76929e7ba8ac9ab7354b9344b589698d9c
Sandeep Sharma <sks34246@gmail.com>
2022-06-16 12:24:23 +0530

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div>
+			Footer
+			<img src='' alt='' />
+		</div>
+	);
+};
+
+export default Footer;
---
+++ src/components/Notification.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Notification = () => {
+  return <div className="flex">Notification</div>;
+};
+
+
+<div></div>
+export default Notification;
